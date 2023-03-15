ebc7cbfcb75713f19417ffae9be341d5658a93fc
khurramsagoo <uaf.khurram@gmail.com>
2023-03-15 09:56:30 -0700

    popover

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 import Pass from './Pass';
 import BasicButton from './BasicButton';
 import BreadCmb from './BreadCmb';
+import EmailField from './EmailField';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -13,7 +13,7 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <BreadCmb/>
-    <App />
+    <EmailField />
     <Pass/>
     <BasicButton/>
   </React.StrictMode>
